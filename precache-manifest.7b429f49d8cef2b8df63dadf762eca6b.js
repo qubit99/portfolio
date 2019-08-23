9f6ab16c7f57dbf472f157c16b7d82b176adb538
qubit99 <architranjan9@gmail.com>
2019-08-23 17:01:19 +0530

    Updates

---
+++ precache-manifest.7b429f49d8cef2b8df63dadf762eca6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ecf82697e88137c1cbcf9c266f97260",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "47efa29a3dbea30f67ca",
+    "url": "/portfolio/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3a97c536c795144f4da3",
+    "url": "/portfolio/static/css/main.088772d9.chunk.css"
+  },
+  {
+    "revision": "47efa29a3dbea30f67ca",
+    "url": "/portfolio/static/js/2.eee9be50.chunk.js"
+  },
+  {
+    "revision": "3a97c536c795144f4da3",
+    "url": "/portfolio/static/js/main.8454d8e6.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
